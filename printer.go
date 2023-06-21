openshift/check-payload

commit b3d831c7845a2488bda50a7714c4c9f14ecfba97

feat: add operator detection
Fixes PR#7

printer.go
@@ -8,12 +8,13 @@ import (
 )
 
 var (
+	colTitleOperatorName = "Operator Name"
 	colTitleTagName      = "Tag Name"
 	colTitleExeName      = "Executable Name"
 	colTitlePassedFailed = "Status"
 	colTitleImage        = "Image"
-	failureRowHeader     = table.Row{colTitleTagName, colTitleExeName, colTitlePassedFailed, colTitleImage}
-	successRowHeader     = table.Row{colTitleTagName, colTitleExeName, colTitleImage}
+	failureRowHeader     = table.Row{colTitleOperatorName, colTitleExeName, colTitlePassedFailed, colTitleImage}
+	successRowHeader     = table.Row{colTitleOperatorName, colTitleExeName, colTitleImage}
 )
 
 var (
@@ -128,9 +129,9 @@ func renderReport(results []*ScanResults) (failures table.Writer, successes tabl
 	for _, result := range results {
 		for _, res := range result.Items {
 			if res.Error != nil {
-				failureTableRows = append(failureTableRows, table.Row{res.Tag.Name, res.Path, res.Error, res.Tag.From.Name})
+				failureTableRows = append(failureTableRows, table.Row{res.OperatorName, res.Tag.Name, res.Path, res.Error, res.Tag.From.Name})
 			} else {
-				successTableRows = append(successTableRows, table.Row{res.Tag.Name, res.Path, res.Tag.From.Name})
+				successTableRows = append(successTableRows, table.Row{res.OperatorName, res.Tag.Name, res.Path, res.Tag.From.Name})
 			}
 		}
 	}

main.go
@@ -58,7 +58,7 @@ var requiredGolangSymbols = []string{
 var Commit string
 
 func main() {
-	var operatorImage = flag.String("operator-image", "", "only run scan on operator image")
+	var containerImage = flag.String("container-image", "", "only run scan on operator image")
 	var components = flag.String("components", "", "scan a specific set of components")
 	var fromFile = flag.String("file", defaultPayloadFilename, "json file for payload")
 	var fromUrl = flag.String("url", "", "http URL to pull payload from")
@@ -84,16 +84,16 @@ func main() {
 	}
 
 	config := Config{
-		FromFile:      *fromFile,
-		FromURL:       *fromUrl,
-		Limit:         *limit,
-		NodeScan:      *nodeScan,
-		OperatorImage: *operatorImage,
-		OutputFile:    *outputFile,
-		OutputFormat:  *outputFormat,
-		Parallelism:   *parallelism,
-		TimeLimit:     *timeLimit,
-		Verbose:       *verbose,
+		FromFile:       *fromFile,
+		FromURL:        *fromUrl,
+		Limit:          *limit,
+		NodeScan:       *nodeScan,
+		ContainerImage: *containerImage,
+		OutputFile:     *outputFile,
+		OutputFormat:   *outputFormat,
+		Parallelism:    *parallelism,
+		TimeLimit:      *timeLimit,
+		Verbose:        *verbose,
 	}
 
 	if *components != "" {

node_scan.go
@@ -84,7 +84,7 @@ func runNodeScan(ctx context.Context, cfg *Config) []*ScanResults {
 				continue
 			}
 			klog.InfoS("scanning path", "path", path, "mtype", mtype.String())
-			res := scanBinary(ctx, tag, cfg.NodeScan, path)
+			res := scanBinary(ctx, "node", tag, cfg.NodeScan, path)
 			if res.Error == nil {
 				klog.InfoS("scanning node success", "path", path, "status", "success")
 			} else {

podman.go
@@ -46,6 +46,16 @@ func podmanPull(ctx context.Context, image string) error {
 	return nil
 }
 
+func podmanInspect(ctx context.Context, image string, args ...string) (string, error) {
+	klog.InfoS("podman: inspect", "image", image)
+	cmdArgs := append([]string{"inspect", image}, args...)
+	stdout, _, err := runPodman(ctx, cmdArgs...)
+	if err != nil {
+		return "", err
+	}
+	return stdout.String(), nil
+}
+
 func runPodman(ctx context.Context, args ...string) (bytes.Buffer, bytes.Buffer, error) {
 	klog.InfoS("podman", "args", args)
 	var stdout bytes.Buffer
@@ -58,4 +68,12 @@ func runPodman(ctx context.Context, args ...string) (bytes.Buffer, bytes.Buffer,
 	}
 	return stdout, stderr, nil
 
+}
+
+func getOpenshiftComponentFromImage(ctx context.Context, image string) (string, error) {
+	component, err := podmanInspect(ctx, image, "--format", "{{index  .Config.Labels \"com.redhat.component\" }}")
+	if err != nil {
+		return "", err
+	}
+	return strings.TrimSpace(component), err
 }

scan.go
@@ -41,7 +41,7 @@ type Result struct {
 }
 
 func Run(ctx context.Context, cfg *Config) []*ScanResults {
-	if cfg.OperatorImage != "" {
+	if cfg.ContainerImage != "" {
 		return runOperatorScan(ctx, cfg)
 	} else if cfg.NodeScan != "" {
 		return runNodeScan(ctx, cfg)
@@ -52,7 +52,7 @@ func Run(ctx context.Context, cfg *Config) []*ScanResults {
 func runOperatorScan(ctx context.Context, cfg *Config) []*ScanResults {
 	tag := &v1.TagReference{
 		From: &corev1.ObjectReference{
-			Name: cfg.OperatorImage,
+			Name: cfg.ContainerImage,
 		},
 	}
 
@@ -208,6 +208,11 @@ func validateTag(ctx context.Context, tag *v1.TagReference, cfg *Config) *ScanRe
 		results.Append(NewScanResult().SetTag(tag).SetError(err))
 		return results
 	}
+	// get openshift component
+	component, _ := getOpenshiftComponentFromImage(ctx, image)
+	if component != "" {
+		klog.Infof("found operator", "component", component)
+	}
 	defer func() {
 		podmanUnmount(ctx, createID)
 	}()
@@ -239,7 +244,7 @@ func validateTag(ctx context.Context, tag *v1.TagReference, cfg *Config) *ScanRe
 			return nil
 		}
 		klog.InfoS("scanning path", "path", path)
-		res := scanBinary(ctx, tag, mountPath, path)
+		res := scanBinary(ctx, component, tag, mountPath, path)
 		if res.Error == nil {
 			klog.InfoS("scanning success", "image", image, "path", printablePath, "status", "success")
 		} else {

types.go
@@ -8,18 +8,19 @@ import (
 )
 
 type Config struct {
-	Components    []string      `json:"components"`
-	Filter        []string      `json:"filter"`
-	FromFile      string        `json:"from_file"`
-	FromURL       string        `json:"from_url"`
-	Limit         int           `json:"limit"`
-	NodeScan      string        `json:"node_scan"`
-	OperatorImage string        `json:"operator_image"`
-	OutputFile    string        `json:"output_file"`
-	OutputFormat  string        `json:"output_format"`
-	Parallelism   int           `json:"parallelism"`
-	TimeLimit     time.Duration `json:"time_limit"`
-	Verbose       bool          `json:"verbose"`
+	Components              []string      `json:"components"`
+	Filter                  []string      `json:"filter"`
+	FromFile                string        `json:"from_file"`
+	FromURL                 string        `json:"from_url"`
+	Limit                   int           `json:"limit"`
+	NodeScan                string        `json:"node_scan"`
+	ContainerImageComponent string        `json:"container_image_component"`
+	ContainerImage          string        `json:"container_image"`
+	OutputFile              string        `json:"output_file"`
+	OutputFormat            string        `json:"output_format"`
+	Parallelism             int           `json:"parallelism"`
+	TimeLimit               time.Duration `json:"time_limit"`
+	Verbose                 bool          `json:"verbose"`
 }
 
 type ArtifactPod struct {
@@ -28,10 +29,11 @@ type ArtifactPod struct {
 }
 
 type ScanResult struct {
-	Tag   *v1.TagReference
-	Path  string
-	Skip  bool
-	Error error
+	OperatorName string
+	Tag          *v1.TagReference
+	Path         string
+	Skip         bool
+	Error        error
 }
 
 type ScanResults struct {

types_config.go
@@ -13,7 +13,7 @@ func (c *Config) Log() {
 		"from_url", c.FromURL,
 		"limit", c.Limit,
 		"node_scan", c.NodeScan,
-		"operator_image", c.OperatorImage,
+		"container_image", c.ContainerImage,
 		"output_file", c.OutputFile,
 		"output_format", c.OutputFormat,
 		"parallelism", c.Parallelism,

types_scan_result.go
@@ -10,6 +10,11 @@ func NewScanResult() *ScanResult {
 	return &ScanResult{}
 }
 
+func (r *ScanResult) SetOperator(operator string) *ScanResult {
+	r.OperatorName = operator
+	return r
+}
+
 func (r *ScanResult) Success() *ScanResult {
 	r.Error = nil
 	return r

commit 462d419d341c54587b91e32e2167729dc108c6ef

printer: fix missing header column
Fixes: b3d831c7845a248
Signed-off-by: Kir Kolyshkin <kolyshkin@gmail.com>

printer.go
@@ -10,11 +10,12 @@ import (
 
 var (
 	colTitleOperatorName = "Operator Name"
+	colTitleTagName      = "Tag Name"
 	colTitleExeName      = "Executable Name"
 	colTitlePassedFailed = "Status"
 	colTitleImage        = "Image"
-	failureRowHeader     = table.Row{colTitleOperatorName, colTitleExeName, colTitlePassedFailed, colTitleImage}
-	successRowHeader     = table.Row{colTitleOperatorName, colTitleExeName, colTitleImage}
+	failureRowHeader     = table.Row{colTitleOperatorName, colTitleTagName, colTitleExeName, colTitlePassedFailed, colTitleImage}
+	successRowHeader     = table.Row{colTitleOperatorName, colTitleTagName, colTitleExeName, colTitleImage}
 )
 
 var (