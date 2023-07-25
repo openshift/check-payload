572c697aa7a3a47c3c58368be2e66018702172a9
Kir Kolyshkin <kolyshkin@gmail.com>
2023-07-25 09:30:08 -0700

    test: check that embedded configs are sane
    The test checks that
     - configs can be parsed
     - there are no unknown/unparsed parts
    
    Signed-off-by: Kir Kolyshkin <kolyshkin@gmail.com>

--- dist/releases/embeds.go
+++ dist/releases/embeds.go
@@ -9,20 +9,27 @@ import (
 //go:embed */*
 var configs embed.FS
 
-func GetConfigFor(version string) ([]byte, error) {
-	bytes, err := configs.ReadFile(filepath.Join(version, "config.toml"))
-	if err == nil {
-		return bytes, nil
-	}
-	// Add a list of valid versions to the error message.
+// GetVersions returns the list of versions for those embedded configs
+// are available.
+func GetVersions() []string {
 	dirs, err := configs.ReadDir(".")
 	if err != nil { // Should not happen.
-		return nil, fmt.Errorf("internal error: %w", err)
+		return nil
 	}
 	names := make([]string, len(dirs))
 	for i, d := range dirs {
 		names[i] = d.Name()
 	}
 
-	return nil, fmt.Errorf("embedded config for version %s is not available; use one of %+v", version, names)
+	return names
+}
+
+// GetConfigFor returns the configuration for a given version, if found.
+func GetConfigFor(version string) ([]byte, error) {
+	bytes, err := configs.ReadFile(filepath.Join(version, "config.toml"))
+	if err == nil {
+		return bytes, nil
+	}
+
+	return nil, fmt.Errorf("embedded config for version %s is not available; use one of %+v", version, GetVersions())
 }
